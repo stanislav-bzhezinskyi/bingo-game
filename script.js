28deccf75d9cdd5e7c9b45329ce6459f4de2351d
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2024-12-27 22:39:36 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const root = document.querySelector(".root");
+const button = document.querySelector(".button");
+
+let numberArray = [];
+let randomNum = () => Math.floor(Math.random() * 46) + 1;
+
+root.className = 'endGame';
+root.textContent = 'lets begin'
+
+button.addEventListener("click", () => {
+  root.className = 'root';
+  randomNum();
+  numberCreator();
+});
+
+
+button.addEventListener('mousedown', () => {
+  button.classList.add('no-shadow');
+  
+})
+button.addEventListener('mouseup', () => {
+  button.classList.remove('no-shadow');
+})
+
+function numChecking(num) {
+  if (numberArray.length === 0) {
+    numberArray.push(num);
+  } else {
+    if (!numberArray.includes(num)) {
+      numberArray.push(num);
+    } else {
+      
+      numberCreator();
+    }
+  }
+}
+
+function numberCreator() {
+  let newNum = randomNum();
+
+  numChecking(newNum);
+
+  if(numberArray.length >= 46) {
+    root.className = 'endGame';
+    button.disabled = true;
+    return root.textContent = "THE END";
+  }
+  
+  root.textContent = numberArray[numberArray.length - 1];
+  
+  console.log(numberArray);
+}
+
+
+
+
