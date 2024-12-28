b921b0ca91453cdc3a216387069585ff464e849a
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2024-12-28 17:05:37 -0600

    add numbers to 48

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const root = document.querySelector(".root");
 const button = document.querySelector(".button");
+const app = document.querySelector('.body');
+
 
 let numberArray = [];
 let randomNum = () => Math.floor(Math.random() * 46) + 1;
@@ -7,26 +9,32 @@ let randomNum = () => Math.floor(Math.random() * 46) + 1;
 root.className = 'endGame';
 root.textContent = 'Lets Begin';
 
+// #region screen height control
 function setAppHeight() {
-  const app = document.getElementById('app');
+  const app = document.getElementById('body');
   app.style.height = `${window.innerHeight}px`;
 }
 
 setAppHeight();
 window.addEventListener('resize', setAppHeight);
-
+// #endregion
+// #region button click
 button.addEventListener("click", () => {
   button.disabled = true;
   root.className = 'root';
+  
+
+  
 
   setTimeout(() => {
     button.disabled = false;
   }, 3000);
+
   randomNum();
   numberCreator();
 });
-
-
+// #endregion
+// #region button shadow
 button.addEventListener('mousedown', () => {
   button.classList.add('no-shadow');
   
@@ -34,7 +42,8 @@ button.addEventListener('mousedown', () => {
 button.addEventListener('mouseup', () => {
   button.classList.remove('no-shadow');
 })
-
+// #endregion
+// #region adding number to array
 function numChecking(num) {
   if (numberArray.length === 0) {
     numberArray.push(num);
@@ -49,13 +58,14 @@ function numChecking(num) {
     }
   }
 }
-
+// #endregion
+// #region number creator
 function numberCreator() {
   let newNum = randomNum();
 
   numChecking(newNum);
 
-  if(numberArray.length >= 46) {
+  if(numberArray.length >= 48) {
     root.className = 'endGame';
     button.disabled = true;
     return root.textContent = "THE END";
@@ -63,13 +73,12 @@ function numberCreator() {
   
   root.textContent = numberArray[numberArray.length - 1];
   
-  console.log(numberArray);
 }
-
+// #endregion
+// #region speaker text
 function speakText(text) {
   const utterance = new SpeechSynthesisUtterance(text);
   utterance.lang = "en-EN"; 
   speechSynthesis.speak(utterance);
 }
-
-
+// #endregion
